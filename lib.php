f9565829dd5c68f36506003829283605fbff0acc
Hussain <hussain.yusuf95@gmail.com>
2025-02-26 15:40:07 +0300

    Add lib file and test

---
+++ lib.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle Course Rollover Plugin
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     mycourses
+ * @author      Husain Mohamed
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @var stdClass $plugin
+ */
+
+
+function add_mycourses_to_menuitem()
+{
+    die('add_mycourses_to_menuitem');
+}
